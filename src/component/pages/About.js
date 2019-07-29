1b60ae072695bf8ab4625ece26063de37581cb0e
rereactjs <reactjs.github@gmail.com>
2019-07-29 18:27:48 +0530

    about Page

--- src/component/pages/About.js
+++ src/component/pages/About.js
@@ -2,8 +2,20 @@ import React from "react";
 
 function About() {
   return (
-    <div>
-      <span>About</span>
+    <div className="container py-5">
+      <div className="jumbotron">
+        <h1 className="display-4">Hello, world!</h1>
+        <p className="lead">
+          This is a simple hero unit, a simple jumbotron-style component for
+          calling extra attention to featured content or information.
+        </p>
+        <hr className="my-4" />
+        <p>
+          It uses utility classes for typography and spacing to space content
+          out within the larger container.
+        </p>
+        <button className="btn btn-primary btn-lg">Learn more</button>
+      </div>
     </div>
   );
 }
